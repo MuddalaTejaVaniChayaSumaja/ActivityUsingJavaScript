a5c2572fd0d2afdd8fb391edfe86aad442e6b6ac
TejaMuddala <125990079+MuddalaTejaVaniChayaSumaja@users.noreply.github.com>
2023-07-19 10:29:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function sleeping(){
+    document.getElementById("act").src="https://img.freepik.com/premium-vector/boy-sleeping-his-bed_723224-371.jpg";
+}
+function playing(){
+    document.getElementById("act").src="https://illustoon.com/photo/8052.png";
+}
+function eating(){
+    document.getElementById("act").src="https://e7.pngegg.com/pngimages/649/438/png-clipart-eating-euclidean-the-boy-eats-child-food.png";
+}
